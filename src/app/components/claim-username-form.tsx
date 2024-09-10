0a020109ecf1fa225c24ef0bbd23ca15ccbae27f
Gui Silva <adrianon10@gmail.com>
2024-09-10 03:38:39 -0300

    feat: add claim username styles component

--- src/app/components/claim-username-form.tsx
+++ src/app/components/claim-username-form.tsx
@@ -1,17 +1,47 @@
 'use client'
 
 import { ArrowRight } from 'lucide-react'
-import { Box, Button, TextInput } from '@ignite-ui/react'
+import { Box, Button, Text, TextInput } from '@ignite-ui/react'
+import { useForm } from 'react-hook-form'
+import {
+  claimUsernameValidation,
+  ClaimUsernameValidationData,
+} from '../validations/claim-username-validation'
+import { zodResolver } from '@hookform/resolvers/zod'
 
 export const ClaimUsernameForm: React.FC = () => {
+  const {
+    formState: { errors },
+    handleSubmit,
+    register,
+  } = useForm<ClaimUsernameValidationData>({
+    resolver: zodResolver(claimUsernameValidation),
+  })
+
+  const handleClaimUsername = ({ username }: ClaimUsernameValidationData) => {
+    console.log(username)
+  }
+
   return (
-    <Box className="grid grid-cols-1 sm:grid-cols-2 gap-2 mt-4 p-4">
-      {/* @ts-expect-error: ERROR */}
-      <TextInput size="sm" prefix="call.one/" placeholder="seu-usuário" />
-      <Button size="sm" type="submit">
-        Reservar
-        <ArrowRight />
-      </Button>
-    </Box>
+    <>
+      <form onSubmit={handleSubmit(handleClaimUsername)}>
+        <Box className="grid grid-cols-1 sm:grid-cols-2 gap-2 mt-4 p-4">
+          {/* @ts-expect-error: ERROR */}
+          <TextInput
+            size="sm"
+            prefix="call.one/"
+            placeholder="seu-usuário"
+            {...register('username')}
+          />
+          <Button size="sm" type="submit">
+            Reservar
+            <ArrowRight />
+          </Button>
+        </Box>
+      </form>
+      <div className="mt-2">
+        <Text size="sm">{errors.username?.message}</Text>
+      </div>
+    </>
   )
 }
