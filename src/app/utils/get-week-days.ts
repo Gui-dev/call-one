29c68c1c2bfd9e0cec9211078445193eb10f8973
Gui Silva <adrianon10@gmail.com>
2024-10-04 05:09:30 -0300

    feat: add short property

--- src/app/utils/get-week-days.ts
+++ src/app/utils/get-week-days.ts
@@ -1,9 +1,17 @@
-export const getWeekDays = () => {
+interface IGetWeekDaysParams {
+  short?: boolean
+}
+
+export const getWeekDays = ({ short = false }: IGetWeekDaysParams = {}) => {
   const formatter = new Intl.DateTimeFormat('pt-BR', { weekday: 'long' })
 
   return Array.from(Array(7).keys())
     .map((day) => formatter.format(new Date(Date.UTC(2021, 5, day))))
     .map((weekDay) => {
+      if (short) {
+        return weekDay.substring(0, 3).toLocaleUpperCase()
+      }
+
       return weekDay
         .substring(0, 1)
         .toLocaleUpperCase()
